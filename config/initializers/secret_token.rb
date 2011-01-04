b0bad8ebe6857970095f86af78ce5ae8a2be184a
justin <justin@JUSTIN-PC.internal.electrum.co.uk>
2011-01-04 17:16:42 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anno::Application.config.secret_token = 'c3af8c58c0a1481a29e892954ba6c320f8e6dcf0736b0a31f07c2d1e8452db72df58c4a45374dde9d4316ba89287456a93c40a8099a484b778d26c8e2efdba0a'
